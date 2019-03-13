4bf4478cee1585f54524151ac0d84e21ad2ca1ce
xiebaichuan <baichuan.xie@gezeal.com>
2019-03-13 09:28:07 +0800

    开房钻石计算逻辑

--- application/controller/Room.php
+++ application/controller/Room.php
@@ -42,7 +42,7 @@ class Room extends Base
         $oldDiamond = $needDiamond; # 房费
         $roomOptions = json_decode($clubGameOpt['options']); # 具体规则
         $roomType =  $clubGameOpt['room_type']; # 房间类型
-        $roomRate =  $clubGameOpt['room_rate']; # 房间算力
+        $roomRate =  $clubGameOpt['room_rate']; # 扣开房人员钻石的规则
         $cheat = $clubGameOpt['cheat']; # 作弊
 
         $tbPlay = new TbPlay();
@@ -61,48 +61,54 @@ class Room extends Base
             return jsonRes(3999);
         }
 
-        $clubType = $clubInfo['club_type'];
+        $clubType = $clubInfo['club_type']; # 开房房费结算类型 0扣开房人员  1扣会长
         $presidentId = $clubInfo['president_id'];
 
-        # 此处代码需要优化 基本完全copy过来  只分析了一下逻辑
 
-        if($clubType == 0){ # 扣用户资产 判断用户资产是否充足
-            $bindingDiamondNum = 0; # 绑定钻石数
-            $type = 10002;
-            $bindingDiamondInfo = getUserProperty($userSessionInfo['userid'], $type);
-            if($bindingDiamondInfo && isset($bindingDiamondInfo[0]['property_num'])){
-                $bindingDiamond = $bindingDiamondInfo[0]['property_num'];
+        if($clubType == 0){ # 扣用户资产 判断用户资产是否充足  玩家扣款中不可能出现免费房间，所以rate<0无需处理
+            if($roomRate == 0){ # AA扣款，房费均摊
+                $needDiamond  = $needDiamond/4; # 逻辑是这样代码需要改
             }
 
-            $diamondNum = 0; # 非绑定钻石数
+            # 获取非绑定钻石数 判断是否能够开房
+            $diamondNum = 0;
             $type = 10001;
             $diamondInfo = getUserProperty($userSessionInfo['userid'], $type);
             if($diamondInfo && isset($diamondInfo[0]['property_num'])){
                 $diamondNum = $diamondInfo[0]['property_num'];
             }
-            $userAllDiamond = bcadd($bindingDiamondNum, $diamondNum, 0); # 玩家全部钻石
 
-            # 玩家扣款中不可能出现免费房间，所以rate<0无需处理
-            if($roomRate == 0){ # AA扣款，房费均摊
+            if($diamondNum < $needDiamond){ # 非绑定钻石不够 获取绑定钻石 判断是是否能够开房
+                $bindingDiamondNum = 0;
+                $type = 10002;
+                $bindingDiamondInfo = getUserProperty($userSessionInfo['userid'], $type);
+                if($bindingDiamondInfo && isset($bindingDiamondInfo[0]['property_num'])){
+                    $bindingDiamondNum = $bindingDiamondInfo[0]['property_num'];
+                }
 
-            }
+                if($bindingDiamondNum < $needDiamond){ # 绑定钻石不够 判断钻石总和是否能够开房
+                    $userAllDiamond = bcadd($bindingDiamondNum, $diamondNum, 0);
+                    if($userAllDiamond < $needDiamond){ # 绑定钻石非绑定钻石相加不够 计算折扣 判断是否能够开房
+                        $discount = 1;
+                        $tbUserVip = new TbUserVip();
+                        $userVipInfo = $tbUserVip->getInfoByUserIdAndClubId($userSessionInfo['userid'], $clubId);
+                        if($userVipInfo){
+                            $vipCardId = $userVipInfo['vid'];
+                            $tbVipCard = new TbVipCard();
+                            $vipCardInfo = $tbVipCard->getInfoById($vipCardId);
+                            if($vipCardInfo){
+                                $discount = bcdiv($vipCardInfo['diamond_consumption'], 100, 1);
+                            }
+                        }
+                        $needDiamond = bcmul($discount, $needDiamond, 0);
 
-            # 获取玩家折扣相关数据
-            $tbUserVip = new TbUserVip();
-            $userVipInfo = $tbUserVip->getInfoByUserIdAndClubId($playerId, $clubId);
-            $discount = 1; # 默认不打折
-            if($userVipInfo){
-                $vipCardId = $userVipInfo['vid'];
-                # 获取折扣数据
-                $tbVipCard = new TbVipCard();
-                $vipCardInfo = $tbVipCard->getInfoById($vipCardId);
-                if($vipCardInfo){
-                    $discount = bcdiv($vipCardInfo['diamond_consumption'], 100, 1);
+                        if($userAllDiamond < $needDiamond){ # 折扣后总钻石还不够 返回错误
+                            $resData['need_diamond'] = $needDiamond;
+                            return jsonRes(23401, $resData);
+                        }
+                    }
                 }
             }
-
-            // TODO - process table options like collation etc
-            //判断玩家是否能够开房
         }
 
 
@@ -116,11 +122,11 @@ class Room extends Base
             }
             if($userDiamond < $needDiamond){
                 $resData['need_diamond'] = $needDiamond;
-                return jsonRes(23401);
+                return jsonRes(23401, $resData);
             }
         }
 
-        ## 此处代码需要优化
+
 
         # 生成房间号
         $redis = new Redis();
