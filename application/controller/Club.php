22d194a35a81631d7351c58585b5149c385752d1
yangtengfei <talyangtf@163.com>
2019-04-09 15:10:48 +0800

    修改club

--- application/controller/Club.php
+++ application/controller/Club.php
@@ -552,7 +552,8 @@ class Club extends Base
                 $diamond = $val['diamond'];
             }
             $diamond = $diamond/$playSize;
-            $game_info['room_cost'] = $diamond;
+            $room_cost = $diamond;
+            //大赢家模式
             if($val['room_rate'] == 1){
                 $room_cost = $val['diamond'];
             }
