4838e110d7b746cea3675a7132b132a0da17014a
xiebaichuan <baichuan.xie@gezeal.com>
2019-06-14 15:49:50 +0800

    谢百川修改结算bug，bug导致代码异常并退出执行

--- application/controller/DisBandCallBack.php
+++ application/controller/DisBandCallBack.php
@@ -114,21 +114,6 @@ class DisBandCallBack extends Base
         // 报送结束
 
         // Todo 助手报送
-        $winnerInfos = $this->opt['statistics'];
-        $gameEndScore = [];
-        foreach ($winnerInfos as $k => $v){
-            $gameEndScore[$v['playerId']] = $v['totalScore'];
-        }
-        $maxScore = max($gameEndScore);
-        foreach ($winnerInfos as $k => $v){
-            if($v['totalScore'] == $maxScore){
-                $winnerInfos[$k]['isWinner'] = 1;
-            }else{
-                $winnerInfos[$k]['isWinner'] = 0;
-            }
-        }
-
-
         $userIds = [];
         $userScore = [];
         foreach ($this->opt['statistics'] as $k => $v){
@@ -144,7 +129,24 @@ class DisBandCallBack extends Base
             }
         }
         $baoSong['opt'] = $this->opt;
-        $baoSong['winnerInfos'] = $winnerInfos;
+
+        if($this->opt['round']){
+            $winnerInfos = $this->opt['statistics'];
+            $gameEndScore = [];
+            foreach ($winnerInfos as $k => $v){
+                $gameEndScore[$v['playerId']] = $v['totalScore'];
+            }
+            $maxScore = max($gameEndScore);
+            foreach ($winnerInfos as $k => $v){
+                if($v['totalScore'] == $maxScore){
+                    $winnerInfos[$k]['isWinner'] = 1;
+                }else{
+                    $winnerInfos[$k]['isWinner'] = 0;
+                }
+            }
+            $baoSong['winnerInfos'] = $winnerInfos;
+        }
+        
         if($userIds){
             $zhushou = [
                 'type' => 'common',
